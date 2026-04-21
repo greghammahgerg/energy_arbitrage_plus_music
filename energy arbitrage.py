2851465b87055bed1f2b5394c8a06706c97a00dc
Greg Hammontree <ghammontree1@gmail.com>
2026-04-20 21:58:17 -0400

    simplified logic

--- energy arbitrage.py
+++ energy arbitrage.py
@@ -1,46 +1,41 @@
 import gridstatus
-import pandas as pd
 import webbrowser
 import time
 
-nyiso = gridstatus.NYISO()
 
-threshold = 100 
+nyiso = gridstatus.NYISO()
+threshold = 52
 spike_music = "https://www.youtube.com/watch?v=3fCEaeqaX6I&list=OLAK5uy_kil-neI15ZGAqwdT4ucB7kEWF4UQj_5f8&index=5"
 spiked = False
-current_price = None
+
 
 print("Let's check the energy prices.")
 
 while True:
     try:
         df = nyiso.get_lmp(date="latest", market="REAL_TIME_5_MIN", locations=["N.Y.C."])
-        spikes = df[df["LMP"] > threshold]
+        current_price = df["LMP"].iloc[0]
         print("Checking for price spikes...")
         
-        if not spikes.empty:
-            current_price = spikes["LMP"].iloc[0]
+
+        if current_price >= threshold:           
             print(f"Price spike detected: ${current_price:.2f}")
-            print("switch to battery power, if you know what's good for ya")
+            print("Use battery power, if you know what's good for ya")
             spiked = True
             webbrowser.open(spike_music)
             time.sleep(300) 
-
-        else:
-            if spiked: 
-                print(f"Prices have stabilized. Current NYC: ${current_price:.2f}")
-                spiked = False
-            else:
-                print(f"Prices are stable (below {threshold}) Current NYC: ${df['LMP'].iloc[0]:.2f}")
-                print('Checking again in 5 minutes.')
-                time.sleep(300)
-                
-
+        
+        elif current_price < threshold:
+            print(f"Prices are stable (below {threshold}) Current NYC: ${current_price:.2f}")
+            print('Checking again in 5 minutes.')
+            time.sleep(300)
+            
     except Exception as e:
         print(f"Error occurred: {e}")
-        time.sleep(60) 
-
+        time.sleep(60)
+    
 
 
 #adjust threshold as needed
-#add relay switch, battery, and connect to computer
+#add relay switch, battery, and connect to a computer (like a raspberry pi)
+>>>>>>> a1e4e92 (simplified logic)
